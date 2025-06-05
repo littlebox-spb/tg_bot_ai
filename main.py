c00c40659031f6e3237b66e4d61def6c4177d2d0
Alexander Altukhov <sasha@littlebox.spb.ru>
2025-06-05 14:57:49 +0300

    Structure

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 import logging
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
+
 from config import TG_BOT_KEY
+from telebot import TeleBot
+from telebot.types import Message
+
+bot = TeleBot(TG_BOT_KEY)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
+
+@bot.message_handler(commands=["random"])
+def start_cmd(message: Message):
+    pass
+
+
+@bot.message_handler(commands=["gpt"])
+def help_cmd(message: Message):
+    pass
+
+
+@bot.message_handler(commands=["talk"])
+def help_cmd(message: Message):
+    pass
+
+
+@bot.message_handler(commands=["quiz"])
+def help_cmd(message: Message):
+    pass
+
+
 def main():
     try:
-        application = Application.builder().token(TG_BOT_KEY).build()
-        application.add_handler(CommandHandler("start", start)) 
-        application.add_handler(CommandHandler("random", start)) 
-
-
+        bot.infinity_polling()
         logger.info("Бот запущен")
-        application.run_polling()
-
     except Exception as e:
         logger.error(f"Ошибка: {e}")
 
